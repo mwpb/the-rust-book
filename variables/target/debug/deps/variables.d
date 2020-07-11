1e1f21602dc8c6ea8d0ada1081234943745123a4
Matthew Burke <matthewpatrickburke@gmail.com>
2020-07-11 12:59:51 -0600

    Ignore target folders

--- variables/target/debug/deps/variables.d
+++
@@ -1,5 +0,0 @@
-C:\Users\matth\learn-rust\variables\target\debug\deps\variables.exe: src\main.rs
-
-C:\Users\matth\learn-rust\variables\target\debug\deps\variables.d: src\main.rs
-
-src\main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-C:\Users\matth\learn-rust\variables\target\debug\variables.exe: C:\Users\matth\learn-rust\variables\src\main.rs
